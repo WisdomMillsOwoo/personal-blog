63b524bf5e6480dd01765cb269ec5edced0b53ab
Wisdom Mills-Owoo <wizmillso@gmail.com>
2025-04-22 20:20:18 -0400

    upgrading components to more recent version of svelte and the necessary dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,22 @@
-import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-auto";
-import { resolve } from "path";
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import {resolve} from "path";
 
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: preprocess(),
-  vite: {
-    resolve: {
-      alias: {
-        $data: resolve("./src/data"),
-      },
-    },
-  },
-  kit: {
-    adapter: adapter(),
-    alias: {
-      $lib: "src/lib",
-      $data: "src/data",
-    },
-  },
+	preprocess: vitePreprocess(),
+	vite: {
+		resolve: {
+			alias: {
+				$data: resolve("./src/data"),
+			},
+		},
+	},
+	kit: {
+		adapter: adapter(),
+		alias: {
+			$lib: "src/lib",
+			$data: "src/data",
+		},}
 };
 
 export default config;
